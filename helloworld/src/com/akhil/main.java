2122f357154e6c42078ef57ab7068a18e9fd552f
akhil <akhiljp5352@gmail.com>
2025-11-16 13:04:28 +0530

    variable use

--- helloworld/src/com/akhil/main.java
+++ helloworld/src/com/akhil/main.java
@@ -2,6 +2,7 @@ package com.akhil;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        int age = 30;
+        System.out.println(age);
     }
 }
